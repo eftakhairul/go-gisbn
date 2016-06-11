3259d2e0f940d7f1c2845e87156854e3169ff7c2
Eftakhairul Islam <eftakhairul@gmail.com>
2016-06-10 23:55:22 -0400

    ISBN_10 and ISBN_13 and thumbnail image methods added

--- gisbn/book.go
+++ gisbn/book.go
@@ -5,14 +5,26 @@ import (
 	"fmt"
 )
 
+type ImageLinks struct {
+	SmallThumbnail  string   `json:"smallThumbnail"`
+	Thumbnail 		string   `json:"thumbnail"`
+}
+
+type IndustryIdentifier struct {
+	Type  			string   `json:"type"`
+	Identifier 		string   `json:"identifier"`
+}
+
 type Book struct {
-	Title       string   `json:"title"`
-	Authors     []string `json:"authors"`
-	Publisher   string   `json:"publisher"`
-	Description string   `json:"description"`
-	Page_count  int      `json:"pageCount"`
-	Type        string   `json:"printType"`
-	Categories  []string `json:"categories"`
+	Title       		string   				`json:"title"`
+	Authors     		[]string 				`json:"authors"`
+	Publisher   		string   				`json:"publisher"`
+	Description 		string   				`json:"description"`
+	Page_count  		int      				`json:"pageCount"`
+	Type        		string   				`json:"printType"`
+	Categories  		[]string 				`json:"categories"`
+	Images				ImageLinks  			`json:"imageLinks"`
+	IndustryIdentifiers []IndustryIdentifier	`json:"industryIdentifiers"`		
 }
 
 type Item struct {
@@ -34,4 +46,35 @@ func getBook(body []byte) (Book, error) {
 	}
 
 	return jsonresponse.Items[0].Volumeinfo, err
+}
+
+
+func (b *Book)ISBN_13() string {
+	if len(b.IndustryIdentifiers) > 0 && b.IndustryIdentifiers[0].Type == "ISBN_13" {
+		return b.IndustryIdentifiers[0].Identifier
+	}
+
+	if len(b.IndustryIdentifiers) > 0 && b.IndustryIdentifiers[1].Type == "ISBN_13" {
+		return b.IndustryIdentifiers[1].Identifier
+	}
+
+	return "Sorry  not found"
+}
+
+
+func (b *Book)ISBN_10() string {
+	if len(b.IndustryIdentifiers) > 0 && b.IndustryIdentifiers[0].Type == "ISBN_10" {
+		return b.IndustryIdentifiers[0].Identifier
+	}
+
+	if len(b.IndustryIdentifiers) > 0 && b.IndustryIdentifiers[1].Type == "ISBN_10" {
+		return b.IndustryIdentifiers[1].Identifier
+	}
+
+	return "Sorry  not found"
+}
+
+func (b *Book)ThumbnailLink() string {
+	
+	return b.Images.Thumbnail
 }
